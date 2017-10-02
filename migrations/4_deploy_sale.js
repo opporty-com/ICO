f64712ab95dd5f6656efcf420af4cc329689d53f
Emil Didnyk <dudnyk@singree.com>
2017-10-02 15:54:36 +0300

    fix truffle migration deploy_sale

--- migrations/4_deploy_sale.js
+++ migrations/4_deploy_sale.js
@@ -19,7 +19,7 @@ module.exports = function(deployer, network) {
           .then(() => OpportySale.deployed());
       })
       .then((instanceOppSale) => {
-        let contractABI = abi.rawEncode(['address', 'address', 'uint', 'uint', 'tokenAddress'], [tokenAddress, walletAddress, start, end, tokenAddress]);
+        let contractABI = abi.rawEncode(['address', 'address', 'uint', 'uint', 'address'], [tokenAddress, walletAddress, start, end, tokenAddress]);
 
         console.log('\n\n\nOpportySaleInfo\n');
         console.log('tokenAddress:  ', tokenAddress);
