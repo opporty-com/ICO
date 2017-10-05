a0d4cfe7e25448ac2f7bc2c715b522b7752b59f3
Emil Didnyk <dudnyk@singree.com>
2017-10-05 16:18:29 +0300

    Hold Contract fix

--- contracts/OpportyHold.sol
+++ contracts/OpportyHold.sol
@@ -3,50 +3,50 @@ pragma solidity ^0.4.15;
 import "./OpportyToken.sol";
 
 contract OpportyHold  {
-	// Addresses and contracts
-	address public OppToken;
-	address public postFreezeDestination;
+  // Addresses and contracts
+  address public OppToken;
+  address public postFreezeDestination;
 
-	// Freezer Data
-	uint public firstAllocation;
+  // Freezer Data
+  uint public firstAllocation;
 
-	uint public firstThawDate;
+  uint public firstThawDate;
 
-	bool public firstUnlocked;
+  bool public firstUnlocked;
 
-	function OpportyHold(
-		address _OppToken,
-		address _postFreezeDestination,
+  function OpportyHold(
+    address _OppToken,
+    address _postFreezeDestination,
         uint firstDate
-	) {
-		OppToken = _OppToken;
-		postFreezeDestination = _postFreezeDestination;
+  ) {
+    OppToken = _OppToken;
+    postFreezeDestination = _postFreezeDestination;
 
-		firstThawDate = now + firstDate * 1 days;  // One year from now
-		
-		firstUnlocked = false;
-	}
+    firstThawDate = now + firstDate * 1 days;  // One year from now
 
-	function getBalance() constant returns (uint)
+    firstUnlocked = false;
+  }
+
+  function getBalance() constant returns (uint)
     {
       return OpportyToken(OppToken).balanceOf(this);
     }
 
-	function unlockFirst() external {
-		require (!firstUnlocked);
-		require (msg.sender == postFreezeDestination);
-		require (now >= firstThawDate);
+  function unlockFirst() external {
+    require (!firstUnlocked);
+    require (msg.sender == postFreezeDestination);
+    require (now >= firstThawDate);
 
-		firstUnlocked = true;
+    firstUnlocked = true;
 
-		uint totalBalance = OpportyToken(OppToken).balanceOf(this);
+    uint totalBalance = OpportyToken(OppToken).balanceOf(this);
 
-		OpportyToken(OppToken).transfer(msg.sender, totalBalance);
-	}
+    OpportyToken(OppToken).transfer(msg.sender, totalBalance);
+  }
 
-	function changeDestinationAddress(address _newAddress) external {
-		require (msg.sender == postFreezeDestination);
-		postFreezeDestination = _newAddress;
-	}
+  function changeDestinationAddress(address _newAddress) external {
+    require (msg.sender == postFreezeDestination);
+    postFreezeDestination = _newAddress;
+  }
   
 }
