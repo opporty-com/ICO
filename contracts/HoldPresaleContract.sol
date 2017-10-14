ee1437aa9ce192c2ce997fc3e46bfce19f56f125
wa1one <vladimir.kovalchuk.88@gmail.com>
2017-10-15 00:12:10 +0300

    hold presale contract

--- contracts/HoldPresaleContract.sol
+++ contracts/HoldPresaleContract.sol
@@ -5,56 +5,76 @@ import "./OpportyToken.sol";
 contract HoldPresaleContract  {
   // Addresses and contracts
   address public OppToken;
-  address public postFreezeDestination;
+
+  struct Holder {
+    bool isActive;
+    uint tokens;
+    uint8 holdPeriod;
+    bool withdrawed;
+  }
+
+  mapping(address => Holder) public holderList;
+  mapping(uint => address) private holderIndexes;
+  uint private holderIndex;
+
+  uint private startDate;
 
   // Freezer Data
-  uint public firstAllocation;
-  uint public firstThawDate;
-  bool public firstUnlocked;
+  uint firstDate;
+  uint secondDate;
+  uint thirdDate;
+  uint fourthDate;
+
+  event TokensTransfered(address contributor , uint amount);
 
   function OpportyHold(
     address _OppToken,
     address _postFreezeDestination,
-        uint firstDate
+    uint start
   ) {
-    OppToken = _OppToken;
-    postFreezeDestination = _postFreezeDestination;
+    OppToken = OpportyToken(_OppToken);
 
-    firstThawDate = now + firstDate * 1 days;  // One year from now
-
-    firstUnlocked = false;
+    startDate = start;
+    firstDate = startDate.add(1 month);
+    secondDate = startDate.add(3 month);
+    thirdDate = startDate.add(6 month);
+    fourthDate = startDate.add(12 month);
   }
-
-
 
   function addHolder(address holder, uint tokens, uint time) {
-    
+    if (holderList[holder].isActive == false) {
+      holderList[holder].isActive = true;
+      holderList[holder].tokens = tokens;
+      holderList[holder].holdPeriod = time;
+      holderIndexes[holderIndex] = holder;
+      holderIndex++;
+    } else {
+      holderList[holder].tokens = tokens;
+      holderList[holder].holdPeriod = time;
+    }
   }
-
-
-
-
 
   function getBalance() constant returns (uint)
-    {
-      return OpportyToken(OppToken).balanceOf(this);
-    }
-
-  function unlockFirst() external {
-    require (!firstUnlocked);
-    require (msg.sender == postFreezeDestination);
-    require (now >= firstThawDate);
-
-    firstUnlocked = true;
-
-    uint totalBalance = OpportyToken(OppToken).balanceOf(this);
-
-    OpportyToken(OppToken).transfer(msg.sender, totalBalance);
+  {
+    return OpportyToken(OppToken).balanceOf(this);
   }
 
-  function changeDestinationAddress(address _newAddress) external {
-    require (msg.sender == postFreezeDestination);
-    postFreezeDestination = _newAddress;
+  function unlockTokens() external {
+    address contributor = msg.sender;
+    bool tosent = false;
+
+    if (holderList[contributor].isActive && !holderList[contributor].withdrawed) {
+      if ( holderList[contributor].holdPeriod == 1 && now > firstDate) tosent = true;
+      if ( holderList[contributor].holdPeriod == 3 && now > secondDate) tosent = true;
+      if ( holderList[contributor].holdPeriod == 6 && now > thirdDate) tosent = true;
+      if ( holderList[contributor].holdPeriod == 12 && now > fourthDate) tosent = true;
+      if (tosend && OpportyToken(OppToken).transfer(msg.sender, holderList[contributor].tokens)) {
+        holderList[contributor].withdrawed = true;
+        TokensTransfered(contributor,  holderList[contributor].tokens);
+      }
+    } else {
+      revert();
+    }
   }
 
 }
